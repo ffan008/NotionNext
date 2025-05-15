e784fcb76ceadc8269682c5c4ed011e0e2e7d797
ffan008 <fanquanjun@gmail.com>
2025-05-15 16:06:50 +0800

    config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     // 英雄区块导航
     PROXIO_HERO_ENABLE: true, // 开启英雄区
     PROXIO_HERO_TITLE_1: 'Fans的军事基地', // 英雄区文字
-    PROXIO_HERO_TITLE_2: '取势  明道  优术  合众', // 英雄区文字
+    PROXIO_HERO_TITLE_2: '取势    明道    优术    合众', // 英雄区文字
     // 英雄区两个按钮，如果TEXT留空则隐藏按钮
     PROXIO_HERO_BUTTON_1_TEXT: '', // 英雄区按钮
     PROXIO_HERO_BUTTON_1_URL:
