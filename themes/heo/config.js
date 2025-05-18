521d320abfc162518e8e11c063414e66cca8a4de
ffan008 <fanquanjun@gmail.com>
2025-05-18 13:40:47 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -118,9 +118,9 @@ const CONFIG = {
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: '',
+  HEO_SOCIAL_CARD_TITLE_2: '欢迎浏览讨论分享',
+  HEO_SOCIAL_CARD_TITLE_3: '点击留下你的智慧',
+  HEO_SOCIAL_CARD_URL: 'http://fans8.fans/message',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
