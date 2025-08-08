93651a0829f573dbcfaf82e1d05707d0bbb25cf2
ffan008 <fanquanjun@gmail.com>
2025-08-08 16:14:44 +0800

    config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
   SIMPLE_LOGO_IMG: '/Logo.webp',
-  SIMPLE_TOP_BAR: true, // 显示顶栏
+  SIMPLE_TOP_BAR: flase, // 显示顶栏
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
   SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>Work Less. Earn More. Enjoy Life.<br/>/简其事，裕其财，悦其生。</div>',
 
